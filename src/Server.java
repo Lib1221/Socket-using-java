ce8936c06d1ec9145c7d42fa2980b7fc35c47fae
Abredagn_Kebede <162739236+AbredagnKebede@users.noreply.github.com>
2024-12-23 15:03:58 +0300

    socket

---
+++ src/Server.java
@@ -0,0 +1,14 @@
+import java.net.ServerSocket;
+public class Server {
+    private ServerSocket serverScocket;
+    public Server(ServerSocket serverSocket){
+        this.serverSocket = serverSocket;
+    }
+    public void startServer(){
+        try{
+            while(!serverScocket.isClosed()){
+                
+            }
+        }
+    }
+}
