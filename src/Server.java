ef22a4e10c2f63bbea3b5cbc6d8144c707533fd8
Abredagn_Kebede <162739236+AbredagnKebede@users.noreply.github.com>
2024-12-23 15:33:36 +0300

    add method startServer

--- src/Server.java
+++ src/Server.java
@@ -1,14 +1,29 @@
 import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
-    private ServerSocket serverScocket;
+
+    private ServerSocket serverSocket;
+    private Thread thread;
+
     public Server(ServerSocket serverSocket){
         this.serverSocket = serverSocket;
-    }
+}
     public void startServer(){
         try{
-            while(!serverScocket.isClosed()){
-                
+            while(!serverSocket.isClosed()){
+
+                Socket socket = serverSocket.accept();
+                System.out.println("A new client has connected!"); 
+                ClientHandler clienHandler = new ClientHandler();
+
+                thread = new Thread(clienHandler);
+                thread.start();
+
             }
+        }
+        catch(IOException e) {
+            
         }
     }
 }
